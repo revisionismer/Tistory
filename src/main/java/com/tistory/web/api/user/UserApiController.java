752e79fccc4e3f9028ec4519d28d4e9a711c3b72
revisionismer <cherry2137@naver.com>
2023-09-23 19:42:55 +0900

    3. AOP 구현 및 사용자 프로필 사진 업데이트 기능 추가

--- src/main/java/com/tistory/web/api/user/UserApiController.java
+++ src/main/java/com/tistory/web/api/user/UserApiController.java
@@ -8,9 +8,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.tistory.config.auth.PrincipalDetails;
 import com.tistory.domain.user.User;
@@ -18,6 +20,7 @@ import com.tistory.dto.ResponseDto;
 import com.tistory.dto.join.JoinReqDto;
 import com.tistory.dto.join.JoinRespDto;
 import com.tistory.dto.user.UserRespDto.UserInfoRespDto;
+import com.tistory.dto.user.UserRespDto.UserProfileRespDto;
 import com.tistory.service.user.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -45,6 +48,16 @@ public class UserApiController {
 		UserInfoRespDto userInfoRespDto = userService.userInfo(loginUser);
 		
 		return new ResponseEntity<>(new ResponseDto<>(1, "로그인 유저 정보 조회 성공", userInfoRespDto), HttpStatus.OK);
+	}
+	
+	@PutMapping("/s/update/profileImage")
+	public ResponseEntity<?> profileImageUpdate(@AuthenticationPrincipal PrincipalDetails principalDetails, MultipartFile profileImageFile) { // 1-1. html input file의 name 값과 매핑해줘야 한다.(중요)
+		
+		User loginUser = principalDetails.getUser();
+		
+		UserProfileRespDto userProfileRespDto = userService.userProfileUpdate(loginUser.getId(), profileImageFile);
+		
+		return new ResponseEntity<>(new ResponseDto<>(1, "프로필 사진 변경 성공", userProfileRespDto), HttpStatus.OK);
 	}
 
 }
---
+++ src/main/resources/static/tistory/js/app/member.js
@@ -0,0 +1,227 @@
+/**
+ *  member.js -> 주의 : user.js라고 이름 지으면 다운로드된다(인식 안됨)
+ */
+
+var ACCESS_TOKEN = getCookie('access_token');
+
+function getCookie(key) {
+	let result = null;
+	let cookie = document.cookie.split(';');
+	cookie.some(function(item) {
+		item = item.replace(' ', '');
+			
+		let dic = item.split('=');
+		if(key === dic[0]) {
+			result = dic[1];
+			return true;
+		}
+	});
+	return result;
+}
+
+console.log(ACCESS_TOKEN);
+
+// 2023-09-22 : 프로필 이미지 업로드 후 처리 완료.
+if(ACCESS_TOKEN != null) {
+	console.log(ACCESS_TOKEN);
+			
+	var header = ACCESS_TOKEN.split('.')[0];
+	var payload = ACCESS_TOKEN.split('.')[1];
+	var signature = ACCESS_TOKEN.split('.')[2];
+			
+	console.log(header);
+	console.log(payload);
+	console.log(signature);
+			
+	console.log("복호화 : " + Base64.decode(payload));
+			
+	// 2023-02-12 -> 토큰에 있는 payload에 실어논 정보를 Base64로 디코드해서 가져와 세팅(base64.min.js 필요)
+	var data = JSON.parse(Base64.decode(payload));
+
+	console.log(data.username);
+	
+	$.ajax({
+		type : "GET",
+		url : "/api/users/s/info",
+		contentType : "application/json; charset=UTF-8",
+		headers: {
+			"Authorization" : "Bearer " + ACCESS_TOKEN
+		},
+		success : function(res) {
+			console.log(res);	
+			
+			if(res.data.profileImageUrl != null) {
+				var imageUrl = `/upload/${res.data.profileImageUrl}`;
+			
+				$('#dropdown_userImage').attr("src", imageUrl);
+			
+			} else {
+				var imageUrl = `/tistory/images/dog.jpg`;
+				
+				$('#dropdown_userImage').attr("src", imageUrl);
+			}
+	
+		},
+		error : function(res) {
+			console.log(res);
+			alert(res.responseJSON.message);
+			location.href = "/login";
+			return;
+		}
+	});	
+	
+}
+
+
+$(document).ready(function(){
+
+	/**
+	 * 2-1. 회원 정보 조회 
+	 */
+	var userUpdateForm = $("#userUpdateForm").val();
+	
+	if(userUpdateForm != null) {
+		$("#userInfo").ready(function(){
+			
+			if(ACCESS_TOKEN != null) {
+				$.ajax({
+					type : "GET",
+					url : "/api/users/s/info",
+					contentType : "application/json; charset=UTF-8",
+					headers: {
+						"Authorization" : "Bearer " + ACCESS_TOKEN
+					},
+					success : function(res) {
+						console.log(res);	
+						
+						$("#userId").val(res.data.id);
+						$("#username").val(res.data.username);
+						$("#email").val(res.data.email);
+						
+						if(res.data.profileImageUrl != null) {
+							var imageUrl = `/upload/${res.data.profileImageUrl}`;
+						
+							console.log(imageUrl);
+							
+							$('#userProfileImage').attr("src", imageUrl);
+						
+						} else {
+							var imageUrl = `/tistory/images/dog.jpg`;
+							
+							console.log(imageUrl);
+						
+							$('#userProfileImage').attr("src", imageUrl);
+						}
+						
+							
+					},
+					error : function(res) {
+						console.log(res);
+						alert(res.responseJSON.message);
+						location.href = "/login";
+						return;
+					}
+				});	
+				
+			} else {
+				alert("로그인을 해주세요.");
+				location.href = "/login";
+				return;
+			}
+			
+		});
+	}
+		
+	/* 2-2. 비밀번호 체크  */
+	$("#convertPassword_chk").focusout(() => {
+    	passwordSameCheck();
+	});
+	
+	function passwordSameCheck() {
+		let password = $("#password").val();
+    	let convertPassword = $("#convertPassword").val();
+    	let convertPassword_chk = $("#convertPassword_chk").val();
+
+    	if(convertPassword !== convertPassword_chk) {
+    		alert("비밀번호가 일치하지 않습니다.");
+    		$("#convertPassword").val("");
+    		$("#convertPassword_chk").val("");
+    	
+    		$("#convertPassword").focus();
+        	return false;
+    	} else if(password === convertPassword) {
+    		alert("변경할 비밀번호가 현재 비밀번호와 같습니다.");
+    		$("#password").val("");
+    		$("#convertPassword").val("");
+    		$("#convertPassword_chk").val("");
+    		
+    		$("#password").focus();
+    		
+    		return false;
+    	}
+	}
+	
+});
+
+/* 3-1. 이미지 업로드 함수 -> 주의 : 뷰에서 직접 사용할 함수면 $(document).ready(){} 밖에 선언되어야 한다. => 2023-09-21 */
+function profileImageUpload() {
+	
+	console.log("프로필이미지 : " + ACCESS_TOKEN);
+
+	// 3-2. 이미지 업로드 창 띄우기
+	$("#profile-img-input").click();
+	
+	// 3-3. 업로드창에 이벤트가 있을 시 실행
+	$("#profile-img-input").on("change", (e) => {
+		let f = e.target.files[0];
+	
+		console.log(f);
+		
+		// 3-4. 이미지가 아닌 파일은 다시 등록하라고 알러트
+		if(!f.type.match("image.*")) {
+			alert("이미지를 등록해야 합니다.");
+			return;
+		}
+		
+		// 3-5. form 태그 전체 가져오기.
+		let profileImageForm = $("#userProfileImageForm")[0];
+		
+		// 3-6. 3-5로 formData 생성
+		let formData = new FormData(profileImageForm);
+		
+		console.log(formData);
+		
+		$.ajax({
+			type: "put",
+			url: "/api/users/s/update/profileImage",
+			headers: {
+				"Authorization" : "Bearer " + ACCESS_TOKEN
+			},
+			data: formData,
+			contentType: false, // 필수 : x-www-form-urlencoded로 파싱되는 것을 방지
+			processData: false, // 필수 : contentType을 false로 줬을 때 QueryString 자동 설정되는 거 해제.
+			enctype: "multipart/form-data",
+			dataType: "json",
+			success: function(res) { 
+				
+				console.log(res);
+				
+				let reader = new FileReader();
+				reader.onload = (e) => {
+					$("#userProfileImage").attr("src", e.target.result);
+				}
+				reader.readAsDataURL(f);	
+				
+				location.reload();
+				
+				
+			},
+			error: function(error) {  
+				console.log(error);
+
+			}
+		});
+		
+	});
+	
+}
